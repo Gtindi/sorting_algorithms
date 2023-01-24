69e1b6f58537536f2249677fc8e0fa766b76a867
Gtindi <tindigabriel@gmail.com>
2023-01-24 17:39:08 +0300

    Repo Updated

---
+++ 1b-main.c
@@ -0,0 +1,56 @@
+#include "sort.h"
+
+/**
+ * create_listint - Creates a doubly linked list from an array of integers
+ *
+ * @array: Array to convert to a doubly linked list
+ * @size: Size of the array
+ *
+ * Return: Pointer to the first element of the created list. NULL on failure
+ */
+listint_t *create_listint(const int *array, size_t size)
+{
+    listint_t *list;
+    listint_t *node;
+    int *tmp;
+
+    list = NULL;
+    while (size--)
+    {
+        node = malloc(sizeof(*node));
+        if (!node)
+            return (NULL);
+        tmp = (int *)&node->n;
+        *tmp = array[size];
+        node->next = list;
+        node->prev = NULL;
+        list = node;
+        if (list->next)
+            list->next->prev = list;
+    }
+    return (list);
+}
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    listint_t *list;
+
+    int *array;
+    size_t n = 101;
+    array = rand_array(n, 0);
+    list = create_listint(array, n);
+    if (!list)
+        return (1);
+    print_list(list);
+    printf("\n");
+    insertion_sort_list(&list);
+    printf("\n");
+    print_list(list);\
+    check_list(list);
+    return (0);
+}
---
+++ 2-main.c
@@ -0,0 +1,20 @@
+#include "sort.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    int array[] = {19, 1, 0};
+    size_t n = sizeof(array) / sizeof(array[0]);
+
+    print_array(array, n);
+    printf("\n");
+    selection_sort(array, n);
+    printf("\n");
+    print_array(array, n);
+    return (0);
+}
+
